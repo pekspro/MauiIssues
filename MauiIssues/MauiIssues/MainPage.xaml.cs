e766f8acdb37ce87f815c472f53c4a0131b77d68
pekspro <msn@pekspro.com>
2022-04-30 10:32:06 +0200

    Adds issue with Shell.FlyoutBehavior="Locked".

--- MauiIssues/MauiIssues/MainPage.xaml.cs
+++ MauiIssues/MauiIssues/MainPage.xaml.cs
@@ -6,13 +6,5 @@ public partial class MainPage : ContentPage
 	{
 		InitializeComponent();
 	}
-
-	private async void OnOpenDetailsClicked(object sender, EventArgs e)
-	{
-		await Shell.Current.GoToAsync(nameof(DetailsPage), new Dictionary<string, object>()
-		{
-			{ "Data", DateTime.Now.ToString("HH:m:ss") }
-		});
-	}
 }
 
