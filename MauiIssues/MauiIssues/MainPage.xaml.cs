3a30848b210e39da571fc72e49d40908d93ba16d
pekspro <msn@pekspro.com>
2022-05-28 12:28:16 +0200

    Adds sample code for issue 7562.

--- MauiIssues/MauiIssues/MainPage.xaml.cs
+++ MauiIssues/MauiIssues/MainPage.xaml.cs
@@ -1,24 +1,43 @@
-﻿namespace MauiIssues;
+﻿using System.Collections.ObjectModel;
+
+namespace MauiIssues;
 
 public partial class MainPage : ContentPage
 {
-	int count = 0;
+    public MainPage()
+    {
+        InitializeComponent();
 
-	public MainPage()
-	{
-		InitializeComponent();
-	}
+        BindingContext = this;
+    }
 
-	private void OnCounterClicked(object sender, EventArgs e)
-	{
-		count++;
+    public class Issue : BindableObject
+    {
+        public string Title { get; set; }
+        public bool IsRed { get; set; } = true;
+        public bool IsGreen => !IsRed;
 
-		if (count == 1)
-			CounterBtn.Text = $"Clicked {count} time";
-		else
-			CounterBtn.Text = $"Clicked {count} times";
+        public void MakeGreen()
+        {
+            IsRed = false;
+            OnPropertyChanged(nameof(IsGreen));
+            OnPropertyChanged(nameof(IsRed));
+        }
+    }
 
-		SemanticScreenReader.Announce(CounterBtn.Text);
-	}
+    public ObservableCollection<Issue> Issues { get; } = new ObservableCollection<Issue>();
+
+    protected async override void OnAppearing()
+    {
+        base.OnAppearing();
+
+        Issues.Add(new Issue { Title = "Issue 1" });
+        Issues.Add(new Issue { Title = "Issue 2" });
+        Issues.Add(new Issue { Title = "Issue 3" });
+
+        await Task.Delay(4000);
+
+        Issues[0].MakeGreen();
+        Issues[1].MakeGreen();
+    }
 }
-
