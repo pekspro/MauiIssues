1a6073175c05329e954514cd5ce9983c528beadd
pekspro <msn@pekspro.com>
2022-05-29 09:18:45 +0200

    Adds sample code for issue 7580.

--- MauiIssues/MauiIssues/MainPage.xaml.cs
+++ MauiIssues/MauiIssues/MainPage.xaml.cs
@@ -1,4 +1,5 @@
 ﻿using System.Collections.ObjectModel;
+using System.Windows.Input;
 
 namespace MauiIssues;
 
@@ -14,30 +15,34 @@ public partial class MainPage : ContentPage
     public class Issue : BindableObject
     {
         public string Title { get; set; }
-        public bool IsRed { get; set; } = true;
-        public bool IsGreen => !IsRed;
+        public bool IsCompleted { get; set; } = true;
+        public bool IsNotCompleted => !IsCompleted;
 
-        public void MakeGreen()
+        public ICommand CommandToggle { get; }
+
+        public Issue()
         {
-            IsRed = false;
-            OnPropertyChanged(nameof(IsGreen));
-            OnPropertyChanged(nameof(IsRed));
+             CommandToggle = new Command(() => Toggle());
+        }
+
+        public void Toggle()
+        {
+            IsCompleted = !IsCompleted;
+            System.Diagnostics.Debug.WriteLine($"{DateTime.Now.ToString("HH:mm:ss")} Setting {Title} to {IsCompleted}");
+            OnPropertyChanged(nameof(IsCompleted));
+            OnPropertyChanged(nameof(IsNotCompleted));
         }
     }
 
     public ObservableCollection<Issue> Issues { get; } = new ObservableCollection<Issue>();
 
-    protected async override void OnAppearing()
+    protected override void OnAppearing()
     {
         base.OnAppearing();
 
-        Issues.Add(new Issue { Title = "Issue 1" });
-        Issues.Add(new Issue { Title = "Issue 2" });
-        Issues.Add(new Issue { Title = "Issue 3" });
-
-        await Task.Delay(4000);
-
-        Issues[0].MakeGreen();
-        Issues[1].MakeGreen();
+        Issues.Add(new Issue { Title = "Issue 1", IsCompleted = false });
+        Issues.Add(new Issue { Title = "Issue 2", IsCompleted = true });
+        Issues.Add(new Issue { Title = "Issue 3", IsCompleted = false });
+        Issues.Add(new Issue { Title = "Issue 4", IsCompleted = true });
     }
 }
