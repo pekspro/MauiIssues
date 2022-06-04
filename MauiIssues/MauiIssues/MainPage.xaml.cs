332b43e99953146d4d7f9e3bffd9b583bc58071e
pekspro <msn@pekspro.com>
2022-06-04 20:33:28 +0200

    Adds sample code for issue 7757.

--- MauiIssues/MauiIssues/MainPage.xaml.cs
+++ MauiIssues/MauiIssues/MainPage.xaml.cs
@@ -2,23 +2,16 @@
 
 public partial class MainPage : ContentPage
 {
-	int count = 0;
-
 	public MainPage()
 	{
 		InitializeComponent();
+
+        Items = TimeZoneInfo.GetSystemTimeZones().ToList();
+
+        BindingContext = this;
+        
 	}
 
-	private void OnCounterClicked(object sender, EventArgs e)
-	{
-		count++;
-
-		if (count == 1)
-			CounterBtn.Text = $"Clicked {count} time";
-		else
-			CounterBtn.Text = $"Clicked {count} times";
-
-		SemanticScreenReader.Announce(CounterBtn.Text);
-	}
+    public List<TimeZoneInfo> Items { get; }
 }
 
