96cd95172e089b98af5e89df28612f44fcda6c36
pekspro <msn@pekspro.com>
2022-09-09 21:18:34 +0200

    Adds workaround for Android.

--- MauiIssues/MauiIssues/App.xaml.cs
+++ MauiIssues/MauiIssues/App.xaml.cs
@@ -2,10 +2,39 @@
 
 public partial class App : Application
 {
-	public App()
+    Microsoft.Maui.Handlers.IToolbarHandler ToolbarHandler;
+
+    public App()
 	{
 		InitializeComponent();
 
-		MainPage = new AppShell();
+        Microsoft.Maui.Handlers.ToolbarHandler.Mapper.AppendToMapping("mytoolbar", (handler, view) =>
+        {
+            ToolbarHandler = handler;
+            SetupMenuBarColorFix();
+        });
+
+        RequestedThemeChanged += (s, e) => SetupMenuBarColorFix();
+
+        MainPage = new AppShell();
 	}
+
+    public void SetupMenuBarColorFix()
+    {
+        if (ToolbarHandler is null)
+        {
+            return;
+        }
+
+#if ANDROID
+        if (RequestedTheme == AppTheme.Light)
+        {
+            ToolbarHandler.PlatformView.OverflowIcon = Platform.CurrentActivity.GetDrawable(Resource.Drawable.ic_more_vert_24_light);
+        }
+        else
+        {
+            ToolbarHandler.PlatformView.OverflowIcon = Platform.CurrentActivity.GetDrawable(Resource.Drawable.ic_more_vert_24_dark);
+        }
+#endif
+    }
 }
