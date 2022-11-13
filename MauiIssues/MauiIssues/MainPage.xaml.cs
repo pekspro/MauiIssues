9c2aa0471f84f3c286c0ee7ee220024ff15aa77b
pekspro <msn@pekspro.com>
2022-11-13 09:07:24 +0100

    Adds sample code for maui issue 11320.

--- MauiIssues/MauiIssues/MainPage.xaml.cs
+++ MauiIssues/MauiIssues/MainPage.xaml.cs
@@ -2,23 +2,12 @@
 
 public partial class MainPage : ContentPage
 {
-	int count = 0;
-
 	public MainPage()
 	{
 		InitializeComponent();
+
+		BindingContext = this;
 	}
 
-	private void OnCounterClicked(object sender, EventArgs e)
-	{
-		count++;
-
-		if (count == 1)
-			CounterBtn.Text = $"Clicked {count} time";
-		else
-			CounterBtn.Text = $"Clicked {count} times";
-
-		SemanticScreenReader.Announce(CounterBtn.Text);
-	}
+	public List<int> Numbers { get; } = Enumerable.Range(1, 100).ToList();
 }
-
