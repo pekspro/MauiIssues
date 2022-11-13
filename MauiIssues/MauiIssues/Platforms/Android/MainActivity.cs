828cb77db0703cb892efb4a50b7602e0fa9d4710
pekspro <msn@pekspro.com>
2022-11-13 08:05:18 +0100

    Adds sample project for maui issue 11319.

--- MauiIssues/MauiIssues/Platforms/Android/MainActivity.cs
+++ MauiIssues/MauiIssues/Platforms/Android/MainActivity.cs
@@ -1,10 +1,32 @@
 ﻿using Android.App;
+using Android.Content;
 using Android.Content.PM;
 using Android.OS;
+using AndroidX.Core.Content;
 
 namespace MauiIssues;
 
 [Activity(Theme = "@style/Maui.SplashTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize | ConfigChanges.Density)]
 public class MainActivity : MauiAppCompatActivity
 {
+    protected override void OnResume()
+    {
+        base.OnResume();
+
+        PackageManager manager = PackageManager!;
+        PackageInfo info;
+        
+        if (OperatingSystem.IsAndroidVersionAtLeast(33))
+        {
+            info = manager.GetPackageInfo(PackageName!, PackageManager.PackageInfoFlags.Of((int)PackageInfoFlags.MetaData));
+        }
+        else
+        {
+//#pragma warning disable 618
+            info = manager.GetPackageInfo(PackageName!, PackageInfoFlags.MetaData)!;
+//#pragma warning restore 618
+        }
+
+        string version = info.VersionName!;
+    }
 }
