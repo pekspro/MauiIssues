8fb4ad3014eefaa71ecf613ff06880d615d9fed4
pekspro <msn@pekspro.com>
2022-12-14 16:51:14 +0100

    Adds sample code for issue 12106.

--- MauiIssues/MauiIssues/MainPage.xaml.cs
+++ MauiIssues/MauiIssues/MainPage.xaml.cs
@@ -2,23 +2,24 @@
 
 public partial class MainPage : ContentPage
 {
-	int count = 0;
-
 	public MainPage()
 	{
 		InitializeComponent();
+		BindingContext = this;
 	}
 
-	private void OnCounterClicked(object sender, EventArgs e)
+	private bool _IsSet = false;
+
+    public bool IsSet
 	{
-		count++;
-
-		if (count == 1)
-			CounterBtn.Text = $"Clicked {count} time";
-		else
-			CounterBtn.Text = $"Clicked {count} times";
-
-		SemanticScreenReader.Announce(CounterBtn.Text);
+        get
+		{
+            return _IsSet;
+        }
+		set
+		{
+            _IsSet = value;
+            OnPropertyChanged();
+        }
 	}
 }
-
