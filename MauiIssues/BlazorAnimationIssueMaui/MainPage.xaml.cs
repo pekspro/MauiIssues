9378db32f2de70704d68031d1ef2df6e52b0a027
pekspro <msn@pekspro.com>
2023-07-31 15:27:03 +0200

    Adds sample code for issue 49755.

---
+++ MauiIssues/BlazorAnimationIssueMaui/MainPage.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace BlazorAnimationIssueMaui
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ MauiIssues/BlazorAnimationIssueMaui/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace BlazorAnimationIssueMaui
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
