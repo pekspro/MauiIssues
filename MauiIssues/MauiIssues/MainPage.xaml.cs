a620d5bb8340551376f2a80e32e90b1b8c25a292
pekspro <msn@pekspro.com>
2023-10-15 19:18:38 +0200

    Adds sample code for issue 1458.

--- MauiIssues/MauiIssues/MainPage.xaml.cs
+++ MauiIssues/MauiIssues/MainPage.xaml.cs
@@ -1,4 +1,7 @@
-﻿namespace MauiIssues;
+﻿using CommunityToolkit.Maui.Storage;
+using System.Diagnostics;
+
+namespace MauiIssues;
 
 public partial class MainPage : ContentPage
 {
@@ -9,16 +12,53 @@ public partial class MainPage : ContentPage
 		InitializeComponent();
 	}
 
-	private void OnCounterClicked(object sender, EventArgs e)
+	private async void SaveFile(object sender, EventArgs e)
 	{
-		count++;
+        using var stream = new MemoryStream("Hello world"u8.ToArray());
+        var fileSaverResult = await FileSaver.Default.SaveAsync("myfile.bin", stream, default);
+    }
 
-		if (count == 1)
-			CounterBtn.Text = $"Clicked {count} time";
-		else
-			CounterBtn.Text = $"Clicked {count} times";
+    private async void OpenFile(object sender, EventArgs e)
+	{
+        var customFileType = new FilePickerFileType(
+            new Dictionary<DevicePlatform, IEnumerable<string>>
+            {
+                { DevicePlatform.Android, new[] { "application/octet-stream" } }
+            });
 
-		SemanticScreenReader.Announce(CounterBtn.Text);
-	}
+        PickOptions options = new()
+        {
+            PickerTitle = "Select file",
+            FileTypes = customFileType,
+            
+        };
+
+        var result = await FilePicker.Default.PickAsync(options);
+        if (result != null)
+        {
+            try
+            {
+                using var stream = await result.OpenReadAsync();
+
+                using var reader = new StreamReader(stream);
+
+                var text = reader.ReadToEnd();
+
+#if ANDROID
+                Android.Widget.Toast.MakeText(Platform.CurrentActivity, "File was read.", Android.Widget.ToastLength.Short)
+                    .Show();
+#endif
+            }
+            catch(Exception ex)
+            {
+#if ANDROID
+                Android.Widget.Toast.MakeText(Platform.CurrentActivity, "Reading failed: " + ex.Message, Android.Widget.ToastLength.Short)
+                    .Show();
+#endif
+                Debugger.Break();
+                string message = ex.Message;
+            }
+        }
+    }
 }
 
--- MauiIssues/MauiIssues/Platforms/Android/MainApplication.cs
+++ MauiIssues/MauiIssues/Platforms/Android/MainApplication.cs
@@ -1,6 +1,12 @@
 ﻿using Android.App;
 using Android.Runtime;
 
+[assembly: UsesPermission(Android.Manifest.Permission.ReadExternalStorage, MaxSdkVersion = 32)]
+[assembly: UsesPermission(Android.Manifest.Permission.WriteExternalStorage, MaxSdkVersion = 32)]
+[assembly: UsesPermission(Android.Manifest.Permission.ReadMediaAudio)]
+[assembly: UsesPermission(Android.Manifest.Permission.ReadMediaImages)]
+[assembly: UsesPermission(Android.Manifest.Permission.ReadMediaVideo)]
+
 namespace MauiIssues;
 
 [Application]
