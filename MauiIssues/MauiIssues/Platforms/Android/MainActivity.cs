cda8b7094d147dfc4f85691d8d48f55359caca8e
pekspro <msn@pekspro.com>
2026-09-13 12:39:04 +0200

    Updates to .NET 10.

--- MauiIssues/MauiIssues/Platforms/Android/MainActivity.cs
+++ MauiIssues/MauiIssues/Platforms/Android/MainActivity.cs
@@ -6,7 +6,7 @@ using AndroidX.Core.Content;
 
 namespace MauiIssues;
 
-[Activity(Theme = "@style/Maui.SplashTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize | ConfigChanges.Density)]
+[Activity(Theme = "@style/Maui.SplashTheme", MainLauncher = true, LaunchMode = LaunchMode.SingleTop, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize | ConfigChanges.Density)]
 public class MainActivity : MauiAppCompatActivity
 {
     protected override void OnResume()
