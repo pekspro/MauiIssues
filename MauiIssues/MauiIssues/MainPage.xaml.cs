8c957fb8ba96ec789ec3cf2109f78f8ea8de4eab
pekspro <msn@pekspro.com>
2026-09-13 12:22:56 +0200

    Updates to .NET 10.

--- MauiIssues/MauiIssues/MainPage.xaml.cs
+++ MauiIssues/MauiIssues/MainPage.xaml.cs
@@ -9,7 +9,7 @@ public partial class MainPage : ContentPage
 		InitializeComponent();
 	}
 
-	private void OnCounterClicked(object sender, EventArgs e)
+	private async void OnCounterClicked(object? sender, EventArgs e)
 	{
 		count++;
 
